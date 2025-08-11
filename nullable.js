b591006b51d2964a1a155b307a87a3e114a51390
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-11 17:28:15 +0200

    The commit about nullable

---
+++ nullable.js
@@ -0,0 +1,33 @@
+// you are given an object with properties that may contain null or undefined values. 
+// Your task is to implement a function called convertNullableValues that checks if 
+// any of the properties in the object contains null or undefined. If a property is 
+// null convert it to zero and if a property is undefined convert it to an empty string. 
+// Return the modified object.
+
+
+const object ={
+    name:null,
+    age:undefined,
+    location: 'kigali'
+   
+}
+
+const convertNullableValues =(obj)=>{
+
+    for(let key in obj ){
+        if(obj[key]===null){
+            obj[key]=0
+        }
+        else if(obj[key]===undefined){
+            obj[key]=""
+
+        }
+    
+    }
+    return obj
+
+}
+
+const objects=convertNullableValues(object)
+console.log(objects)
+
