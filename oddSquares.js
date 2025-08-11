9cead5686daf15ab962de3b34fb4c4912c348fd2
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-11 16:18:32 +0200

    The commit about to return odd elements in array and squared it

---
+++ oddSquares.js
@@ -0,0 +1,11 @@
+//   You are given an array of numbers. Your task is to implement a function called oddSquares that filters out
+//  the even numbers from the array and then maps the remaining numbers to their squares.
+
+const oddSquares =(arr)=>{
+
+    const odd=arr.filter(num=>num%2!==0)
+    const oddSquares = odd.map(num=>num*num)
+    return oddSquares
+    
+}
+console.log(oddSquares([1, 2, 3, 4, 5, 6]))
