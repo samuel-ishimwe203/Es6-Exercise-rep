86c8eeca324a592ab708241e7a78778db80ce96d
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-13 16:32:31 +0200

    new update

--- removeDuplication.js
+++ removeDuplication.js
@@ -1,7 +1,22 @@
 
+// remove duplication
 const greet=(arr)=>{
-    return arr.filter((el,ind)=>arr.indexOf(el)==ind)
+    return arr.filter((el,ind)=>arr.indexOf(el)===ind)
     
 }
 
 console.log(greet([1,2,3,4,3,1,4,5,6]))
+
+// return duplication
+
+function findDuplicates(arr) {
+  return arr.filter((num, index) => arr.indexOf(num) !== index)
+            .filter((num, index, self) => self.indexOf(num) === index);
+}
+
+console.log(findDuplicates([1,2,3,4,3,1,4,5,6]))
+
+
+
+
+
