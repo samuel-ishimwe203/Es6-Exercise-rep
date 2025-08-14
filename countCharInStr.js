fec54526ec4190a19c46de0f8c7f3ca8ce1d2b07
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-14 15:25:04 +0200

    new changes

--- countCharInStr.js
+++ countCharInStr.js
@@ -16,3 +16,19 @@ console.log(countChrInString('sweeet'))
 
 //   return non-repeated character in string 
 
+function firstNonRepeatingChar(str){
+    
+    const count={}
+    
+    for(let char of str){
+      count[char]=(count[char]||0)+1;
+  
+    }
+    
+    for (let i=0; i<str.length;i++){
+        if(count[str[i]]===1)return str[i]
+    }
+    return null
+}
+
+console.log(firstNonRepeatingChar('sweeyet'))
