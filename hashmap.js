8499554133673cd1178de1f3f0c44bffdbb56b38
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-14 13:53:38 +0200

    new changes

---
+++ hashmap.js
@@ -0,0 +1,11 @@
+
+
+// hash map is data structure of key-value pairs
+const hashMap ={}
+
+hashMap['name']='samuel'
+hashMap['age']=34
+hashMap['location']='kigali'
+
+console.log(hashMap['name'])
+
