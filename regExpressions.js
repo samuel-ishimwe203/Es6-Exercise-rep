8a62c906644524256dd6f78b12008a903839216e
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-16 23:33:22 +0200

    new changes

--- regExpressions.js
+++ regExpressions.js
@@ -1,4 +1,5 @@
 // regular expressions : Define a search pattern that can be used t search for things in a string and also is used to merge part of string
+// i in regular expression is to get string as the same case even if upper or lower
 
 
 
@@ -7,3 +8,15 @@ let sentence ='the dog chased the cat.'
 let regex = /chaseds/
  
 console.log(regex.test(sentence))
+
+// to extract word in string 
+
+let str ="the man is like 'coding' as she's career"
+let reg=/coding/ig;
+console.log(str.match(reg))
+
+// g is going to return every single /repeat/g in sentence while when you use /repeat/ it returns only first repeat in sentence
+
+let string1 = 'repeat , repeat , is , goinng , to , repeat'
+let regEx = /repeat/ig
+console.log(string1.match(regEx))
