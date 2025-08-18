4da675a3f6fe98cdbae77c415e53dcee595d5804
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-18 12:04:32 +0200

    new changes

---
+++ squareSum.js
@@ -0,0 +1,13 @@
+/*
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because  */
+
+
+function squareSum(numbers){
+  
+  return numbers.map(num=>num*num).reduce((acc,curr)=>acc+curr,0)
+
+}
+
+console.log(squareSum([1,2,4]))
