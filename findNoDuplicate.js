f1d8d366b47e3801547b2ffc4fe2bd7fb8b5819a
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-21 11:12:39 +0200

    new changes

---
+++ findNoDuplicate.js
@@ -0,0 +1,9 @@
+
+function findUnique(numbers) {
+    // Return the unique number
+  const array = numbers.filter((ele,ind)=>numbers.indexOf(ele)===numbers.lastIndexOf(ele))
+  
+  return array.join()
+}
+
+console.log(findUnique([1,2,3,4,5,6,1,2,3,4,5]))
