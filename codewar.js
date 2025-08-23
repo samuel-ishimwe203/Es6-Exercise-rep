8adf58d5f50dabd6d2fdb38724343377db7f43d3
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-23 21:14:48 +0200

    new changes

---
+++ codewar.js
@@ -0,0 +1,22 @@
+function arrayToString(arr) {
+  function helper(element) {
+    if (Array.isArray(element)) {
+      // Recursively process nested arrays
+      return "[" + element.map(helper).join(",") + "]";
+    } 
+    else if (typeof element === "string") {
+      // Wrap strings with single quotes
+      return `'${element}'`;
+    } 
+    else if (typeof element === "boolean" || typeof element === "number") {
+      // Numbers & booleans stay as-is
+      return String(element);
+    } 
+    else {
+      // Fallback (in case of unexpected values)
+      return String(element);
+    }
+  }
+
+  return "[" + arr.map(helper).join(",") + "]";
+}
