baaf6d8e52d4c3a72a05e78265e86e92dbe0cd82
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-30 20:46:13 +0200

    new changes

---
+++ sum.js
@@ -0,0 +1,18 @@
+//  return the sum of positive number and count number of negative
+
+
+function greet(arr){
+    
+    const positive= arr.filter(num=>num>0)
+    const leng = positive.length
+    const negative=arr.filter(num=>num<0).reduce((acc,curr)=>acc+curr,0)
+   
+
+
+    return leng
+
+
+    
+}
+
+console.log(greet([1,4,5,6,-3,-7,-8]))
