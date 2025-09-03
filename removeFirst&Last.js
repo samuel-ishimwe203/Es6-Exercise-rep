4b9f0171c506a685b7c3a3f1f925c3bde7724078
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-09-03 17:29:01 +0200

    new changes

---
+++ removeFirst&Last.js
@@ -0,0 +1,10 @@
+
+function greet(string){
+    if(!string)return null;
+
+    const arr =string.split("")
+    if(arr.length<=2) return null;
+    return arr.slice(1,-1).join('')
+}
+
+console.log(greet('samuel'))
