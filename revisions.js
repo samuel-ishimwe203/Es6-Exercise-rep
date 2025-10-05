885cceeaf89c48384011ba085dcd88bde1942800
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-10-05 14:42:05 +0200

    .

--- revisions.js
+++ revisions.js
@@ -2,8 +2,18 @@
 
 const greet= (arr)=>{
 
-    const array = arr.filter(num=>num<=0).reduce((acc,curr)=>acc+curr,0)
-    return array
+    const array1 = arr.filter(num=>num<=0).reduce((acc,curr)=>acc+curr,0)
+     const array2 = arr.filter(num=>num>0).length
+    return [array1,array2]
 }
 
 console.log(greet([1,2,-4,-3,5,-6]))
+
+// Find the shortest word in a string
+
+const findShort= (str)=>{
+    return str.split(' ').reduce((shortest, current)=>{
+      return current.length< shortest.length ? current : shortest
+    })
+}
+console.log(findShort("I am the youngest man in our home"))
