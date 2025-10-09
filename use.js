3946ef8df057b4987fc0a16bbc637910f63d72aa
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-10-09 19:36:46 +0200

    .

---
+++ use.js
@@ -0,0 +1,14 @@
+function useMe (arr) {
+    const duplicates = arr.filter((el, index, self) => self.indexOf(el) !== self.lastIndexOf(el))
+    const sortedArr = arr.sort()
+    const missing = []
+    for (let index = 0; index < sortedArr.length; index++) {
+        const element = sortedArr[index];
+        if (sortedArr[index + 1] - element > 1) {
+            missing.push(element + 1)
+        }
+    }
+    return [...missing, duplicates[1]]
+}
+
+console.log(useMe([2,3,4,1]))
